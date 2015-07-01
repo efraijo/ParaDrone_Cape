4a91429574a68dedec3cd39d38b69a1485840d03
Ihab Salameh <isalameh@ucsd.edu>
2015-07-01 16:03:35 -0700

    deleted jerry's uncommented code

--- paraglide-master/global.h
+++
@@ -1,8 +0,0 @@
-#ifndef GLOBAL_H
-#define GLOBAL_H
-
-#define SAMPLE_HZ 50
-
-
-
-#endif /*GLOBAL_H*/
--- paraglide-master/task.h
+++
@@ -1,6 +0,0 @@
-#ifndef TASK_H
-#define TASK_H
-
-int task_sample_imu();
-int task_pause_unpress();
-#endif /*TASK_H*/
